7e94a36eb199df5ba25939b1d245dff301bd646e
weideng <wei.a.deng@intel.com>
2020-08-20 10:14:44 +0800

    Add unit test cases for Get_version/Version
    Signed-off-by: weideng <wei.a.deng@intel.com>

--- UnitTest/TestSpdmResponder/TestSpdmResponderVersion.c
+++ UnitTest/TestSpdmResponder/TestSpdmResponderVersion.c
@@ -77,6 +77,81 @@ void TestSpdmResponderVersionCase2(void **state) {
   assert_int_equal (SpdmResponse->Header.Param2, 0);
 }
 
+void TestSpdmResponderVersionCase3(void **state) {
+  RETURN_STATUS        Status;
+  SPDM_TEST_CONTEXT    *SpdmTestContext;
+  SPDM_DEVICE_CONTEXT  *SpdmContext;
+  UINTN                ResponseSize;
+  UINT8                Response[MAX_SPDM_MESSAGE_BUFFER_SIZE];
+  SPDM_VERSION_RESPONSE *SpdmResponse;
+
+  SpdmTestContext = *state;
+  SpdmContext = &SpdmTestContext->SpdmContext;
+  SpdmTestContext->CaseId = 0x3;
+  SpdmContext->ResponseState = SpdmResponseStateBusy;
+
+  ResponseSize = sizeof(Response);
+  Status = SpdmGetResponseVersion (SpdmContext, mSpdmGetVersionRequest1Size, &mSpdmGetVersionRequest1, &ResponseSize, Response);
+  assert_int_equal (Status, RETURN_SUCCESS);
+  assert_int_equal (ResponseSize, sizeof(SPDM_ERROR_RESPONSE));
+  SpdmResponse = (VOID *)Response;
+  assert_int_equal (SpdmResponse->Header.RequestResponseCode, SPDM_ERROR);
+  assert_int_equal (SpdmResponse->Header.Param1, SPDM_ERROR_CODE_BUSY);
+  assert_int_equal (SpdmResponse->Header.Param2, 0);
+  assert_int_equal (SpdmContext->ResponseState, SpdmResponseStateBusy);
+}
+
+void TestSpdmResponderVersionCase4(void **state) {
+  RETURN_STATUS        Status;
+  SPDM_TEST_CONTEXT    *SpdmTestContext;
+  SPDM_DEVICE_CONTEXT  *SpdmContext;
+  UINTN                ResponseSize;
+  UINT8                Response[MAX_SPDM_MESSAGE_BUFFER_SIZE];
+  SPDM_VERSION_RESPONSE *SpdmResponse;
+
+  SpdmTestContext = *state;
+  SpdmContext = &SpdmTestContext->SpdmContext;
+  SpdmTestContext->CaseId = 0x4;
+  SpdmContext->ResponseState = SpdmResponseStateNeedResync;
+
+  ResponseSize = sizeof(Response);
+  Status = SpdmGetResponseVersion (SpdmContext, mSpdmGetVersionRequest1Size, &mSpdmGetVersionRequest1, &ResponseSize, Response);
+  assert_int_equal (Status, RETURN_SUCCESS);
+  assert_int_equal (ResponseSize, sizeof(SPDM_ERROR_RESPONSE));
+  SpdmResponse = (VOID *)Response;
+  assert_int_equal (SpdmResponse->Header.RequestResponseCode, SPDM_ERROR);
+  assert_int_equal (SpdmResponse->Header.Param1, SPDM_ERROR_CODE_REQUEST_RESYNCH);
+  assert_int_equal (SpdmResponse->Header.Param2, 0);
+  assert_int_equal (SpdmContext->ResponseState, SpdmResponseStateNormal);
+}
+
+void TestSpdmResponderVersionCase5(void **state) {
+  RETURN_STATUS        Status;
+  SPDM_TEST_CONTEXT    *SpdmTestContext;
+  SPDM_DEVICE_CONTEXT  *SpdmContext;
+  UINTN                ResponseSize;
+  UINT8                Response[MAX_SPDM_MESSAGE_BUFFER_SIZE];
+  SPDM_VERSION_RESPONSE *SpdmResponse;
+  SPDM_ERROR_DATA_RESPONSE_NOT_READY *ErrorData;
+
+  SpdmTestContext = *state;
+  SpdmContext = &SpdmTestContext->SpdmContext;
+  SpdmTestContext->CaseId = 0x5;
+  SpdmContext->ResponseState = SpdmResponseStateNotReady;
+
+  ResponseSize = sizeof(Response);
+  Status = SpdmGetResponseVersion (SpdmContext, mSpdmGetVersionRequest1Size, &mSpdmGetVersionRequest1, &ResponseSize, Response);
+  assert_int_equal (Status, RETURN_SUCCESS);
+  assert_int_equal (ResponseSize, sizeof(SPDM_ERROR_RESPONSE) + sizeof(SPDM_ERROR_DATA_RESPONSE_NOT_READY));
+  SpdmResponse = (VOID *)Response;
+  ErrorData = (SPDM_ERROR_DATA_RESPONSE_NOT_READY*)(&SpdmResponse->Reserved);
+  assert_int_equal (SpdmResponse->Header.RequestResponseCode, SPDM_ERROR);
+  assert_int_equal (SpdmResponse->Header.Param1, SPDM_ERROR_CODE_RESPONSE_NOT_READY);
+  assert_int_equal (SpdmResponse->Header.Param2, 0);
+  assert_int_equal (SpdmContext->ResponseState, SpdmResponseStateNormal);
+  assert_int_equal (ErrorData->RequestCode, SPDM_GET_VERSION);
+}
+
 SPDM_TEST_CONTEXT       mSpdmResponderVersionTestContext = {
   SPDM_TEST_CONTEXT_SIGNATURE,
   FALSE,
@@ -94,6 +169,12 @@ int SpdmResponderVersionTestMain(void) {
   const struct CMUnitTest SpdmResponderVersionTests[] = {
     cmocka_unit_test(TestSpdmResponderVersionCase1),
     cmocka_unit_test(TestSpdmResponderVersionCase2),
+    // ResponseState: SpdmResponseStateBusy
+    cmocka_unit_test(TestSpdmResponderVersionCase3),
+    // ResponseState: SpdmResponseStateNeedResync
+    cmocka_unit_test(TestSpdmResponderVersionCase4),
+    // ResponseState: SpdmResponseStateNotReady
+    cmocka_unit_test(TestSpdmResponderVersionCase5),
   };
 
   SetupSpdmTestContext (&mSpdmResponderVersionTestContext);
