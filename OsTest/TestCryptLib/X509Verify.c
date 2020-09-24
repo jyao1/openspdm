3e3d819c262fcba5229c2c2d413d242e8173c046
Xiaoyu Lu <xiaoyux.lu@intel.com>
2020-09-24 22:38:26 +0800

    OsTest/TestCryptLib: add x509 related test
    x509 field information get test
    
    Signed-off-by: Xiaoyu Lu <xiaoyux.lu@intel.com>

--- OsTest/TestCryptLib/X509Verify.c
+++ OsTest/TestCryptLib/X509Verify.c
@@ -1,4 +1,4 @@
-/** @file  
+/** @file
 
 Copyright (c) 2020, Intel Corporation. All rights reserved.<BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -30,6 +30,19 @@ ValidateCryptX509 (
   UINTN TestBundleCertLen;
   UINT8 *TestEndCert;
   UINTN TestEndCertLen;
+  UINTN SubjectSize;
+  UINT8 *Subject;
+  UINTN CommonNameSize;
+  CHAR8 CommonName[64];
+  RETURN_STATUS Ret;
+  UINTN CertVersion;
+  UINT8  Asn1Buffer[1024];
+  UINTN  Asn1BufferLen;
+  UINTN DMTFOidSize;
+  UINT8 DMTFOid[64];
+  CONST UINT8 DMTF_OID[] = {
+    0x2B, 0x06, 0x01, 0x4, 0x01, 0x83, 0x1C, 0x82, 0x12, 0x01
+  };
 
 
   Status = ReadInputFile ("test/inter.cert.der", (VOID **)&TestCert, &TestCertLen);
@@ -122,5 +135,136 @@ ValidateCryptX509 (
   }
 
 
+  //
+  // X509 Certificate Subject Retrieving.
+  //
+  Print ("- X509 Certificate Subject Bytes Retrieving ... ");
+  SubjectSize = 0;
+  Status  = X509GetSubjectName (TestCert, TestCertLen, NULL, &SubjectSize);
+  Subject = (UINT8 *)AllocatePool (SubjectSize);
+  Status  = X509GetSubjectName (TestCert, TestCertLen, Subject, &SubjectSize);
+  FreePool(Subject);
+  if (!Status) {
+    Print ("[Fail]");
+    return EFI_ABORTED;
+  } else {
+    Print ("[Pass]");
+  }
+
+  Print ("\n- X509 Certificate Context Retrieving ... ");
+  //
+  // Get CommonName from X509 Certificate Subject
+  //
+  CommonNameSize = 64;
+  ZeroMem (CommonName, CommonNameSize);
+  Ret = X509GetCommonName (TestCert, TestCertLen, CommonName, &CommonNameSize);
+  if (RETURN_ERROR (Ret)) {
+    Print ("\n  - Retrieving Common Name - [Fail]");
+    return EFI_ABORTED;
+  } else {
+    DEBUG((DEBUG_INFO, "\n  - Retrieving Common Name = \"%s\" (Size = %d)", CommonName, CommonNameSize));
+    Print(" - [PASS]");
+  }
+
+  //
+  // Get Issuer OrganizationName from X509 Certificate Subject
+  //
+  CommonNameSize = 64;
+  ZeroMem (CommonName, CommonNameSize);
+  Ret = X509GetOrganizationName (TestCert, TestCertLen, CommonName, &CommonNameSize);
+  if (Ret != RETURN_NOT_FOUND) {
+    Print ("\n  - Retrieving Oraganization Name - [Fail]");
+    return EFI_ABORTED;
+  } else {
+    Print ("\n  - Retrieving Oraganization Name - [PASS]");
+  }
+
+  //
+  // Get Version from X509 Certificate
+  //
+  CertVersion = 0;
+  Ret = X509GetVersion(TestCert, TestCertLen, &CertVersion);
+  if (RETURN_ERROR (Ret)) {
+    Print ("\n  - Retrieving Version - [Fail]");
+    return EFI_ABORTED;
+  } else {
+    DEBUG((DEBUG_INFO, "\n  - Retrieving Version = %d - ", CertVersion));
+    Print ("[Pass]");
+  }
+
+  //
+  // Get Serial from X509 Certificate
+  //
+  Asn1BufferLen = 1024;
+  ZeroMem(Asn1Buffer, Asn1BufferLen);
+  Ret = X509GetSerialNumber(TestCert, TestCertLen, Asn1Buffer, &Asn1BufferLen);
+  if (RETURN_ERROR (Ret)) {
+    Print ("\n  - Retrieving SerialNumber - [Fail]");
+    return EFI_ABORTED;
+  } else {
+    DEBUG((DEBUG_INFO, "\n  - Retrieving SerialNumber = %d - ", *((UINT64*)Asn1Buffer)));
+    Print ("[Pass]");
+  }
+
+  //
+  // X509 Certificate Subject Retrieving.
+  //
+  Print ("\n  - Retrieving issuer Bytes ... ");
+  SubjectSize = 0;
+  Status  = X509GetIssuerName (TestCert, TestCertLen, NULL, &SubjectSize);
+  Subject = (UINT8 *)AllocatePool (SubjectSize);
+  Status  = X509GetIssuerName (TestCert, TestCertLen, Subject, &SubjectSize);
+  FreePool(Subject);
+  if (!Status) {
+    Print ("[Fail]");
+    return EFI_ABORTED;
+  } else {
+    Print (" - [Pass]");
+  }
+
+  //
+  // Get Issuer CommonName from X509 Certificate Subject
+  //
+  CommonNameSize = 64;
+  ZeroMem (CommonName, CommonNameSize);
+  Ret = X509GetIssuerCommonName (TestCert, TestCertLen, CommonName, &CommonNameSize);
+  if (RETURN_ERROR (Ret)) {
+    Print ("\n  - Retrieving Issuer Common Name - [Fail]");
+    return EFI_ABORTED;
+  } else {
+    DEBUG((DEBUG_INFO, "\n  - Retrieving Issuer Common Name = \"%s\" (Size = %d) - ", CommonName, CommonNameSize));
+    Print ("[Pass]");
+  }
+
+  //
+  // Get Issuer OrganizationName from X509 Certificate Subject
+  //
+  CommonNameSize = 64;
+  ZeroMem (CommonName, CommonNameSize);
+  Ret = X509GetIssuerOrganizationName (TestCert, TestCertLen, CommonName, &CommonNameSize);
+  if (Ret != RETURN_NOT_FOUND) {
+    Print ("\n  - Retrieving Issuer Oraganization Name - [Fail]");
+    return EFI_ABORTED;
+  } else {
+    Print ("\n  - Retrieving Issuer Oraganization Name - [Pass]");
+  }
+
+  //
+  // Get X509GetSubjectAltName
+  //
+  CommonNameSize = 64;
+  DMTFOidSize = 64;
+  ZeroMem (CommonName, CommonNameSize);
+  ZeroMem (DMTFOid, DMTFOidSize);
+  Ret = X509GetDMTFSubjectAltName(TestEndCert, TestEndCertLen, CommonName, &CommonNameSize, DMTFOid, &DMTFOidSize);
+  if (RETURN_ERROR(Ret) || CompareMem(DMTFOid, DMTF_OID, sizeof (DMTF_OID)) != 0) {
+    Print ("\n  - Retrieving  SubjectAltName otherName - [Fail]");
+    return EFI_ABORTED;
+  } else {
+    DEBUG((DEBUG_INFO, "\n  - Retrieving  SubjectAltName otherName = \"%s\" (Size = %d) ", CommonName, CommonNameSize));
+    Print ("- [Pass]");
+  }
+
+  Print ("\n");
   return EFI_SUCCESS;
 }
